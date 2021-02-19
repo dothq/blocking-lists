ca653bc81fa3ac393206f6e4efb3dcfcc47f578e
trickypr <23250792+trickypr@users.noreply.github.com>
2021-02-19 12:39:32 +1100

    🔊 Remove lexeme

--- scripts/tokeniser/token.ts
+++ scripts/tokeniser/token.ts
@@ -2,19 +2,19 @@ import TokenType from '../tokenType'
 
 class Token {
   type: TokenType
-  lexeme: string
+  //lexeme: string
   literal: Object
   line: number
 
   constructor(type: TokenType, lexeme: string, literal: Object, line: number) {
     this.type = type
-    this.lexeme = lexeme
+    //this.lexeme = lexeme
     this.literal = literal
     this.line = line
   }
 
   toString() {
-    return `${this.type} ${this.lexeme} ${this.literal}`
+    return `${this.type} {this.lexeme} ${this.literal}`
   }
 }
 
