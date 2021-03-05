d9e513e8a8d3399237032c7bb9ee7ac7805ff6af
trickypr <23250792+trickypr@users.noreply.github.com>
2021-03-05 18:21:36 +1100

    :bug: Remove localhost strings from the block list

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -15,6 +15,7 @@ const process = (contents: string) =>
     .map((str) => str.split(' ')[1] || '')
     .filter((str) => str !== '')
     .filter((str) => str !== '0.0.0.0')
+    .filter(str => str != 'localhost')
     .map((str) => `*://*.${str}/*`)
 
 const file = (cacheFile: string) => {
