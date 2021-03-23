4fa52733f201a17035ed4b82809f01239e64a1f0
TrickyPR <trickypr@icloud.com>
2021-03-23 18:50:34 +1100

    ➕ Add more lists and host support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     env,
     error::Error,
-    fmt::format,
     fs::{self, create_dir, File},
     io::{self, Write},
 };
@@ -11,12 +10,15 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 struct List {
     abp: Vec<String>,
+    hosts: Vec<String>,
 }
 
 #[derive(Debug, Deserialize)]
 struct Config {
     trackers: List,
-    social: Lists,
+    social: List,
+    fake_news: List,
+    gambling: List,
 }
 
 #[tokio::main]
@@ -37,13 +39,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     list(config.trackers, "trackers").await?;
     list(config.social, "social").await?;
+    list(config.fake_news, "fake_news").await?;
+    list(config.gambling, "gambling").await?;
 
     Ok(())
 }
 
 async fn list(list: List, name: &str) -> Result<(), Box<dyn Error>> {
     println!("Downloading lists...");
-    let trackers_list = download_abp(list.abp).await?;
+    let mut trackers_list = download_abp(list.abp).await?;
+    let mut host_list = download_hosts(list.hosts).await?;
+    trackers_list.append(&mut host_list);
+
+    println!("Deduping...");
+    trackers_list.sort();
+    trackers_list.dedup();
 
     println!("Saving...");
     let mut file = File::create(&format!("out/{}.txt", name))?;
@@ -64,6 +74,9 @@ async fn download_abp(lists: Vec<String>) -> Result<Vec<String>, Box<dyn Error>>
             .iter()
             .map(|s| s.split('!').collect::<Vec<&str>>()[0])
             .filter(|s| s != &"")
+            .filter(|s| {
+                !s.contains('<') && !s.contains(' ') && !s.contains('>') && !s.contains('\u{0009}')
+            }) // Why is there html in this list
             .map(|s| s.to_string())
             .collect();
 
@@ -72,9 +85,32 @@ async fn download_abp(lists: Vec<String>) -> Result<Vec<String>, Box<dyn Error>>
         println!("Downloaded: '{}'", &file);
     }
 
-    println!("Deduping...");
-    list.sort();
-    list.dedup();
+    Ok(list)
+}
+
+async fn download_hosts(lists: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
+    // Download files and store them in a scripts
+    let mut list = Vec::new();
+    for file in lists {
+        let downloaded = reqwest::get(&file).await?.text().await?;
+
+        let split: Vec<&str> = downloaded.split('\n').collect();
+
+        let mut split = split
+            .iter()
+            .map(|s| s.split('#').collect::<Vec<&str>>()[0])
+            .filter(|s| s != &"")
+            .filter(|s| s != &"0.0.0.0")
+            .filter(|s| !s.contains("localhost"))
+            .map(|s| str::replace(s, "0.0.0.0 ", ""))
+            .map(|s| str::replace(&s, "127.0.0.1 ", ""))
+            .map(|s| format!("||{}^", s))
+            .collect();
+
+        list.append(&mut split);
+
+        println!("Downloaded: '{}'", &file);
+    }
 
     Ok(list)
 }
