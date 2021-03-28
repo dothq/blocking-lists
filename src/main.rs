52b89c939c9372b6f87a7bfc7eda1258e9271c7d
TrickyPR <trickypr@icloud.com>
2021-03-28 17:46:40 +1100

    🔧 Allow for the specification of custom out folders

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ struct List {
 #[derive(Debug, Deserialize)]
 struct Config {
     name: String,
+    out: String,
     lists: Vec<List>,
 }
 
@@ -43,7 +44,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Loop through all of the lists
     for list_config in config.lists {
         // Parse the list
-        list(&list_config, &mut progress).await?;
+        list(&list_config, &mut progress, &config.out).await?;
         // Increment total progress bar
         progress.inc_and_draw(&total_lists, 1);
     }
@@ -51,7 +52,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn list(list: &List, progress: &mut Progress) -> Result<(), Box<dyn Error>> {
+async fn list(list: &List, progress: &mut Progress, out: &str) -> Result<(), Box<dyn Error>> {
     let name = &list.name;
     let mut full_list = Vec::new();
     let total_lists = list.abp.len() + list.hosts.len() + 2; // We add the extra one for deduping and saving
@@ -79,7 +80,7 @@ async fn list(list: &List, progress: &mut Progress) -> Result<(), Box<dyn Error>
 
     progress.inc_and_draw(&bar, 1);
 
-    let mut file = File::create(&format!("out/{}.txt", name))?;
+    let mut file = File::create(&format!("{}/{}.txt", name, out))?;
     file.write_all(&full_list.join("\n").as_bytes())?;
 
     progress.inc_and_draw(&bar, 1);
